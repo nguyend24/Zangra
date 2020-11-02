3997991055f69362e995dc156be8e50b25b254ac
Dennis Nguyen <dennisdnguyen19@gmail.com>
2020-11-02 04:29:29 -0500

    Add error logging for adding and removing roles

--- src/bin/denny_bot/four_horseman_server/utils.rs
+++ src/bin/denny_bot/four_horseman_server/utils.rs
@@ -14,7 +14,20 @@ static FOUR_HORSEMAN_ROLE_ID: u64 = 715247696663019560;
 //Hardcoded role verification for Preston's Community Server
 pub fn add_role_rules_verified(ctx: &Context, add_reaction: &Reaction) {
     if add_reaction.message_id.as_u64() == &FOUR_HORSEMAN_MESSAGE_ID {
-        let mut reaction_member = ctx.cache.read().member(&add_reaction.guild_id.unwrap(), &add_reaction.user_id).unwrap();
+        let guild_id = match &add_reaction.guild_id {
+            Some(id) => id,
+            None => {
+                println!("add_role_rules_verified - guild id not found");
+                return
+            }
+        };
+        let mut reaction_member = match ctx.cache.read().member(guild_id, &add_reaction.user_id) {
+            Some(member) => member,
+            None => {
+                println!("add_role_rules_verified - member not found");
+                return
+            }
+        };
         let role_to_add = RoleId(FOUR_HORSEMAN_ROLE_ID);
         if let Err(why) = reaction_member.add_role(ctx, role_to_add) {
             println!("Error adding role: {:?}", why)
@@ -24,7 +37,20 @@ pub fn add_role_rules_verified(ctx: &Context, add_reaction: &Reaction) {
 
 pub fn _remove_role_rules_verified(ctx: &Context, remove_reaction: &Reaction) {
     if remove_reaction.message_id.as_u64() == &FOUR_HORSEMAN_MESSAGE_ID {
-        let mut reaction_member = ctx.cache.read().member(&remove_reaction.guild_id.unwrap(), &remove_reaction.user_id).unwrap();
+        let guild_id: &GuildId = match &remove_reaction.guild_id {
+            Some(id) => id,
+            None => {
+                println!("remove_role_rules_verified - guild id not found");
+                return
+            }
+        };
+        let mut reaction_member: Member = match ctx.cache.read().member(&guild_id, &remove_reaction.user_id) {
+            Some(member) => member,
+            None => {
+                println!("remove_role_rules_verified - member not found");
+                return
+            }
+        };
         let role_to_remove: RoleId = RoleId(FOUR_HORSEMAN_ROLE_ID);
         if let Err(why) = reaction_member.remove_role(&ctx.http, role_to_remove) {
             println!("Error removing role: {:?}", why)
