b95a9fe4bd81a7dfefb339fa571dd38cba87b930
Dennis Nguyen <dennisdnguyen19@gmail.com>
2020-11-03 01:10:41 -0500

    Convert event handler to async

--- src/bin/denny_bot/discord_event_handler.rs
+++ src/bin/denny_bot/discord_event_handler.rs
@@ -5,15 +5,17 @@ use serenity::model::prelude::{Reaction, Ready};
 use serenity::prelude::{Context, EventHandler};
 use crate::test_server::{_reaction_add_test, _reaction_remove_test};
 use serenity::model::channel::Message;
+use serenity::async_trait;
 
 pub struct Handler;
 
+#[async_trait]
 impl EventHandler for Handler {
-    fn reaction_add(&self, ctx: Context, reaction: Reaction) {
+    async fn reaction_add(&self, ctx: Context, reaction: Reaction) {
         add_role_rules_verified(&ctx, &reaction);
     }
 
-    fn ready(&self, ctx: Context, ready: Ready) {
+    async fn ready(&self, ctx: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
         println!("Version: {}", VERSION);
 
