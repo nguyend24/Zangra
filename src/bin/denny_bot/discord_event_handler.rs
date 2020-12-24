9c7ea54d94884d76e64b1a1ebd3df4bb50e5ebed
Dennis Nguyen <dennisdnguyen19@gmail.com>
2020-12-23 23:48:22 -0500

    Auto role for member join

--- src/bin/denny_bot/discord_event_handler.rs
+++ src/bin/denny_bot/discord_event_handler.rs
@@ -1,4 +1,4 @@
-use crate::limited_budgetworks_server::utils::add_role_rules_verified;
+use crate::limited_budgetworks_server::utils::{add_role_rules_verified, add_member_join_role};
 use crate::twitch_webhook_handler::set_up_twitch_webhooks;
 use crate::VERSION;
 use serenity::model::prelude::{Reaction, Ready};
@@ -7,12 +7,19 @@ use crate::test_server::{_reaction_add_test, _reaction_remove_test};
 use serenity::model::channel::{Message, Embed};
 use serenity::async_trait;
 use serenity::utils::Color;
-use serenity::model::id::ChannelId;
+use serenity::model::id::{ChannelId, GuildId};
+use serenity::model::guild::Member;
 
 pub struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
+    async fn guild_member_addition(&self, ctx: Context, guild_id: GuildId, new_member: Member) {
+        if guild_id.as_u64() == &713889872359981076 {
+            add_member_join_role(&ctx, new_member).await;
+        }
+    }
+
     async fn reaction_add(&self, ctx: Context, reaction: Reaction) {
         add_role_rules_verified(&ctx, &reaction).await;
     }
@@ -40,4 +47,6 @@ impl EventHandler for Handler {
         };
         // set_up_twitch_webhooks(ctx);
     }
+
+
 }
