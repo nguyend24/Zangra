626635b69c3c8b99e3e93379923452c5b7ba0588
Dennis Nguyen <dennisdnguyen19@gmail.com>
2021-02-12 17:58:13 -0500

    Use a constant value for the default user avatar url

--- src/bin/zangra/edbh/utils.rs
+++ src/bin/zangra/edbh/utils.rs
@@ -4,6 +4,8 @@ use serenity::model::voice::VoiceState;
 use crate::utils::voice::{VoiceStateChange, identify_state};
 use chrono::Utc;
 
+const DEFAULT_AVATAR: &str = "https://www.denofgeek.com/wp-content/uploads/2020/06/Discord.png";
+
 pub async fn voice_state_changed(ctx: &Context, guild_id: &GuildId, old: &Option<VoiceState>, new: &VoiceState) {
     if let Some(state_change) = identify_state(guild_id, &old, &new) {
         match state_change {
@@ -30,7 +32,7 @@ async fn left_voice_channel(ctx: &Context, _guild_id: &GuildId, _old: &Option<Vo
     let name: String = member.user.name.clone();
     let icon_url: String = match member.user.avatar_url() {
         Some(url) => url,
-        None => String::from("https://www.denofgeek.com/wp-content/uploads/2020/06/Discord.png")
+        None => String::from(DEFAULT_AVATAR)
     };
 
     if let Err(why) = ChannelId(805186168647974964).send_message(&ctx, |m| m
@@ -51,7 +53,7 @@ async fn joined_voice_channel(ctx: &Context, _guild_id: &GuildId, new: &VoiceSta
     let name: String = member.user.name.clone();
     let icon_url: String = match member.user.avatar_url() {
         Some(url) => url,
-        None => String::from("https://www.denofgeek.com/wp-content/uploads/2020/06/Discord.png")
+        None => String::from(DEFAULT_AVATAR)
     };
 
     if let Err(why) = ChannelId(805186168647974964).send_message(&ctx, |m| m
