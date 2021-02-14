95ff233f4fdcec9639bf227cac032ecb0756dec0
Dennis Nguyen <dennisdnguyen19@gmail.com>
2021-02-14 02:26:47 -0500

    User moved voice channel functionality

--- src/bin/zangra/edbh/utils.rs
+++ src/bin/zangra/edbh/utils.rs
@@ -15,7 +15,9 @@ pub async fn voice_state_changed(ctx: &Context, guild_id: &GuildId, old: &Option
             VoiceStateChange::JoinedVoiceChannel => {
                 joined_voice_channel(ctx, guild_id, new).await;
             }
-            VoiceStateChange::MovedVoiceChannel => {}
+            VoiceStateChange::MovedVoiceChannel => {
+                moved_voice_channel(ctx, guild_id, old, new).await;
+            }
             VoiceStateChange::ServerDeafened => {}
             VoiceStateChange::ServerMuted => {}
             VoiceStateChange::SelfDeafened => {}
@@ -65,4 +67,34 @@ async fn joined_voice_channel(ctx: &Context, _guild_id: &GuildId, new: &VoiceSta
             .timestamp(Utc::now().to_rfc3339()))).await {
         println!("Error sending BH join message. Why: {}", why);
     };
+}
+
+async fn moved_voice_channel(ctx: &Context, _guild_id: &GuildId, old: &Option<VoiceState>, new: &VoiceState) {
+    let member = new.member.as_ref().unwrap();
+    let name: String = member.user.name.clone();
+    let icon_url: String = match member.user.avatar_url() {
+        Some(url) => url,
+        None => String::from(DEFAULT_AVATAR)
+    };
+
+    let old_channel_id = old.as_ref().unwrap().channel_id.as_ref().unwrap();
+    let new_channel_id = new.channel_id.as_ref().unwrap();
+    let old_channel_name = match old_channel_id.name(ctx).await {
+        Some(name) => name,
+        None => "No channel name found".to_string()
+    };
+    let new_channel_name = match new_channel_id.name(ctx).await {
+        Some(name) => name,
+        None => "No channel name found".to_string()
+    };
+
+    if let Err(why) = ChannelId(805186168647974964).send_message(ctx, |m| m
+        .embed(|e| e
+            .title(format!("{} moved {} -> {}", &name, old_channel_name, new_channel_name))
+            .author(|a| a
+                .name(&name)
+                .icon_url(icon_url))
+            .timestamp(Utc::now().to_rfc3339()))).await {
+        println!("Error sending BH move message. Why: {}", why);
+    }
 }
